6efc2f73bbf2953e1fa62364c01b2efd2c2f727b
ricardoaom15 <ricardoaom15@gmail.com>
2017-06-07 09:28:20 -0500

    User class

---
+++ app/src/main/java/com/creatic/particularteacherprototype/models/User.java
@@ -0,0 +1,89 @@
+package com.creatic.particularteacherprototype.models;
+
+/**
+ * Created by RicardoAndrés on 07/06/2017.
+ */
+
+public class User {
+    long id;
+    String name;
+    String username;
+    String email;
+    String password;
+    String document;
+    String phone;
+    String avatar;
+    String banner;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+}
