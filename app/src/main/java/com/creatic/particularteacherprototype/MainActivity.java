d046a8d0a2bf0d7b975684c4b0232696b3a619ec
ricardoaom15 <ricardoaom15@gmail.com>
2017-06-08 21:52:30 -0500

    ImageSwitcher

--- app/src/main/java/com/creatic/particularteacherprototype/MainActivity.java
+++ app/src/main/java/com/creatic/particularteacherprototype/MainActivity.java
@@ -1,15 +1,24 @@
 package com.creatic.particularteacherprototype;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.creatic.particularteacherprototype.adapters.OfferAdapter;
 import com.creatic.particularteacherprototype.adapters.SubjectAdapter;
@@ -19,11 +28,17 @@ import com.creatic.particularteacherprototype.databinding.ActivityMainBinding;
 import com.creatic.particularteacherprototype.models.Offer;
 import com.creatic.particularteacherprototype.models.Subject;
 import com.creatic.particularteacherprototype.util.L;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements SubjectAdapter.OnItemClick, OfferAdapter.OnItemClick {
+
+    private static final Integer TIMER_DURATION = 2500;
 
     ActivityMainBinding binding;
     SubjectAdapter subjectAdapter;
@@ -36,8 +51,7 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.On
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(binding.toolbar);
 
         L.subjectStaticList = new ArrayList<>();
         subjectDao = new SubjectDao(this);
@@ -50,6 +64,21 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.On
         offerAdapter = new OfferAdapter(this, this, L.offerStaticList);
         binding.mainContent.mainOfferList.setAdapter(offerAdapter);
         binding.mainContent.mainOfferList.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.mainHeaderImg.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.MATCH_PARENT));
+
+                return imageView;
+            }
+        });
+        binding.mainHeaderImg.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        binding.mainHeaderImg.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out));
+
+        startSlider();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -123,5 +152,48 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.On
     @Override
     public void onOfferClick(View v) {
 
+    }
+
+    private void startSlider(){
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Picasso.with(MainActivity.this)
+                                .load(Uri.parse(getRandomImage()))
+                                .into(new Target() {
+                                    @Override
+                                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                                        binding.mainHeaderImg.setImageDrawable(new BitmapDrawable(getResources(), bitmap));
+                                    }
+
+                                    @Override
+                                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                                    }
+
+                                    @Override
+                                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                                    }
+                                });
+                    }
+                });
+            }
+        }, 0, TIMER_DURATION);
+    }
+
+    private String getRandomImage(){
+        String[] urls = {"http://www.artsfon.com/large/201504/68395.jpg",
+                "https://s-media-cache-ak0.pinimg.com/736x/cf/1d/3f/cf1d3fe48d2707aa2c7e8d20d795e6ef.jpg",
+                "http://www.deafacademy2013.com/wp-content/uploads/2017/05/635729543467663355-385689442_tumblr_static_wallpaper__book_by_analaurasam-d6cak0w.imgopt1000x70.jpg"};
+
+        int length = urls.length;
+        int position = (int) (Math.random() * length);
+
+        return urls[position];
     }
 }
